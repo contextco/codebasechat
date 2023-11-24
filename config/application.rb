457732672ee8f96a6fc71ad9eadaa51f5609c727
Alex Gamble <agamble.uk@gmail.com>
2023-11-24 14:50:46 +0000

    actually use sidekiq

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module GitQuestion
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
+
   resources :repos, only: [:index] do
     collection do
       post :action
